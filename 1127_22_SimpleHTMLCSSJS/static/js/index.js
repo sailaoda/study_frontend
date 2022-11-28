e2bbb08b88d2688d1a6bb27b110b4e4bed60ce1d
sailaoda <1520516332@qq.com>
2022-11-28 16:55:03 +0800

    【特性】：完善simple

--- 1127_22_SimpleHTMLCSSJS/static/js/index.js
+++ 1127_22_SimpleHTMLCSSJS/static/js/index.js
@@ -22,7 +22,25 @@ myImage.onclick = function() {
 
 // document.querySelector('h1').textContent = 'Hello World!';
 
-document.querySelector('h1').addEventListener('click', () => {
+/* document.querySelector('h1').addEventListener('click', () => {
 	alert('dfkjlasf');
 });
-document.querySelector('h1').onclick = function(){};
+document.querySelector('h1').onclick = function(){}; */
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myButton.onclick = function() {
+	setUserName();
+ }
+
+
+function setUserName() {
+	let myName = prompt('请输入你的名字。');
+	if (!myName) {
+		setUserName();
+	} else {
+		localStorage.setItem('name', myName);
+		myHeading.textContent = 'Google 太帅啦，' + myName;
+	}
+}
